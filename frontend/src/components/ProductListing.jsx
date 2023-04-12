68dc73a20a267ca8f235474a5f096b954d36eb3b
Ezequiel Reyes <reyeseze@usc.edu>
2023-04-12 01:47:52 -0700

    Product Listing Page

---
+++ frontend/src/components/ProductListing.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./ProductListing.css";
+
+function ProductListingPage() {
+  // Define state variables to hold the product data
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [condition, setCondition] = useState("");
+  const [images, setImages] = useState([]);
+
+  // Define event handlers for form inputs
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleConditionChange = (event) => {
+    setCondition(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    const fileList = event.target.files;
+    const imagesArray = [];
+    for (let i = 0; i < fileList.length; i++) {
+      imagesArray.push(fileList[i]);
+    }
+    setImages(imagesArray);
+  };
+
+  // Define function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the product data to the backend using a POST request
+    // Clear the form inputs and display a success message
+  };
+
+  return (
+    <div className="product-listing-page">
+      <div className = "head">
+        <h1>List a Product!</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <div>
+              Name:
+            </div>
+          <input type="text" value={name} onChange={handleNameChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <div>
+              Description:
+            </div>
+          <textarea value={description} onChange={handleDescriptionChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <div>
+              Condition:
+            </div>
+          <input type="text" value={condition} onChange={handleConditionChange} />
+        </label>
+        </div>
+        <div className = "images">
+          <label>
+            <div>
+              Images:
+            </div>
+          <input type="file" multiple onChange={handleImageChange} />
+        </label>
+        </div>
+        <div className= "submitbttn">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ProductListingPage;
