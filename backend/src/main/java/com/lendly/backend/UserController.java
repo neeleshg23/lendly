26565cc4e7dc2cbe492902c8a617e3cdca2138d8
Neel Gupta <neeleshgupta023@gmail.com>
2023-04-12 14:55:53 -0700

    Adding findOwnedItems() and findBorrowedItems() to API w/ matching postman tests

--- backend/src/main/java/com/lendly/backend/UserController.java
+++ backend/src/main/java/com/lendly/backend/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lendly.backend.model.Item;
 import com.lendly.backend.model.User;
 
 import java.util.List;
@@ -57,6 +58,26 @@ public class UserController {
                 .map(existingUser -> {
                     userRepository.deleteById(id);
                     return ResponseEntity.noContent().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/owned-items")
+    public ResponseEntity<List<Item>> getOwnedItemsByUserId(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    List<Item> ownedItems = userRepository.findOwnedItems(id);
+                    return ResponseEntity.ok(ownedItems);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/borrowed-items")
+    public ResponseEntity<List<Item>> getBorrowedItemsByUserId(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    List<Item> borrowedItems = userRepository.findBorrowedItems(id);
+                    return ResponseEntity.ok(borrowedItems);
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
