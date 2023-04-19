b199adf33c127dc3d89a7cf4306d48df2e3b6871
Ezequiel Reyes <reyeseze@usc.edu>
2023-04-18 22:18:38 -0700

    Added currency input functionality

--- frontend/src/components/ItemListing.jsx
+++ frontend/src/components/ItemListing.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar";
 import "../ItemListing.css";
+import CurrencyInput from "react-currency-input-field";
+import { CurrencyInputProps, CurrencyInputOnChangeValues } from 'react-currency-input-field';
 
 function ProductListingPage({user}) {
+
   // Define state variables to hold the product data
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [condition, setCondition] = useState("");
   const [images, setImages] = useState([]);
+  const prefix = '$';
+  const [value, setValue] = useState(0);
 
   // Define event handlers for form inputs
   const handleNameChange = (event) => {
@@ -31,58 +36,92 @@ function ProductListingPage({user}) {
     setImages(imagesArray);
   };
 
+  // Handler for currency input
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { value = "" } = e.target;
+    const parsedValue = value.replace(/[^\d.]/gi, "");
+    setValue(parsedValue);
+  };
+  // Needed function for currency
+  const handleOnBlur = () => setValue(Number(value).toFixed(2));
+
   // Submit event handler 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault();
     // Send the product data to the backend using a POST request
     // Clear the form inputs and display a success message
+    // const response = await fetch('http://localhost:8080/api/items', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(user),
+    // });
+
+    // if (response.ok) {
+    //   console.log('Listing created successfully');
+    //   navigate('/market');
+    // } else {
+    //   console.error('Error creating item');
+      // Show an error message
+    // }
+
+    // example of shit 
+    //     "id": 8,
+    //     "category": "Kitchenware",
+    //     "insurancePrice": 25.0,
+    //     "status": false,
+    //     "ownerId": 10,
+    //     "borrowerId": 0,
+    //     "name": "Blender"
   };
 
   return (
-    <div className="product-listing-page">
-      <NavBar user={user} />
-      <div className = "head">
-        <h1>List an Item!</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <div>
-              Name:
-            </div>
-          <input type="text" value={name} onChange={handleNameChange} />
-          </label>
-        </div>
-        <div>
-          <label>
-            <div>
-              Description:
-            </div>
-          <textarea value={description} onChange={handleDescriptionChange} />
-          </label>
-        </div>
-        <div>
-          <label>
-            <div>
-              Condition:
-            </div>
-          <input type="text" value={condition} onChange={handleConditionChange} />
-        </label>
-        </div>
-        <div className = "images">
-          <label>
-            <div>
-              Images:
-            </div>
-          <input type="file" multiple onChange={handleImageChange} />
-        </label>
-        </div>
-        <div className= "submitbttn">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div class="search">
+       <form onSubmit={handleSubmit}>
+           <h1>Post an Item!</h1>
+           <label for="keyword">Category:</label>
+           {/* <input type="text" id="keyword" name="keyword" required> */}
+           <select id="category" value={name} onChange={handleNameChange}>
+             <option value="">Select a category</option>
+             <option value="furniture">Furniture</option>
+             <option value="clothing">Clothing</option>
+             <option value="books">Books</option>
+             <option value="electronics">Electronics</option>
+           </select>
+
+
+           <label for="location">Insurance:</label>
+           <CurrencyInput
+            prefix={prefix}
+            name="currencyInput"
+            id="currencyInput"
+            data-number-to-fixed="2"
+            data-number-stepfactor="100"
+            value={value}
+            placeholder=""
+            onChange={handleChange}
+            onBlur={handleOnBlur}
+            allowDecimals
+            decimalsLimit="2"
+            disableAbbreviations
+          />
+           {/* <input value={name} onChange={handleNameChange} /> */}
+  
+           <label for="location">Name:</label>
+           <input value={name} onChange={handleNameChange} />
+
+           <label for="location">Images:</label>
+           <input type="file" multiple onChange={handleImageChange} />
+  
+           <button type="submit">Submit</button>
+       </form>
     </div>
   );
 }
 
 export default ProductListingPage;
+
+
+
