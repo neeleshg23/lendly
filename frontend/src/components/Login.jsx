f172539f1745ec7cc95d9132769f071df1f4e1e0
Rachel Perry <perryrs@usc.edu>
2023-04-19 02:03:55 -0700

    Home, Login, Register, Market

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './../App.css';
+import image from "./../images/snowboots.jpg"
 
 const Login = () => {
     return (
@@ -11,7 +12,7 @@ const Login = () => {
             <div class="two-colored-box">
                 <div class="left-column">
                 {/* content for the left column goes here */}
-                    <img src="snowboots.jpg"/>
+                    <img src={image}/>
                     <h3>trade, lend, wear, share.</h3>
                 </div>
                 <div class="right-column">
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './../App.css';
+import image from "./../images/jacket.jpg"
 
 const Register = () => {
   const [state, setState] = useState({
@@ -56,7 +57,7 @@ const Register = () => {
       <div class="two-colored-box">
           <div class="left-column">
           {/* content for the left column goes here */}
-              <img src="rollerblades.jpg"/>
+              <img src={image}/>
               <h3>trade, lend, wear, share.</h3>
           </div>
           <div className="right-column">
