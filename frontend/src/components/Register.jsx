28b555c2f4d34cebba38db3d76bd6ecda1c6ab48
Neel Gupta <neeleshgupta023@gmail.com>
2023-04-19 23:03:08 -0700

    test deploy 10

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -35,6 +35,7 @@ const Register = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify(user),
     });
