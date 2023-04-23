70e4b4cafc177db64e0bae2661c96ceb7a4d47c4
Jihyun/Blake La <late@usc.edu>
2023-04-22 17:09:52 -0700

    fix 2

--- backend/src/main/java/com/lendly/backend/LendlyBackendApplication.java
+++ backend/src/main/java/com/lendly/backend/LendlyBackendApplication.java
@@ -43,7 +43,7 @@ public class LendlyBackendApplication {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
-                        .maxAge(3600);
+                        .maxAge(3600)
                         .exposedHeaders("Access-Control-Allow-Origin");
             }
         };
