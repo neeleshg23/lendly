5a9c8b904392eaaf8b76b9e6bb0f0712ae80106d
Rachel Perry <perryrs@usc.edu>
2023-04-22 20:09:33 -0700

    (Should) clear items when searching a blank keyword

--- frontend/src/components/MarketItem.jsx
+++ frontend/src/components/MarketItem.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import './../App.css';
+import './../Rachel.css';
 import image from "./../images/rollerblades.jpg"
 
 
-const MarketItem = ({ itemName, itemPrice, itemCategory }) => {
+const MarketItem = ({ user, itemName, itemPrice, itemCategory }) => {
     return (
         <div className="market-item">
             <div className="left-column">
@@ -24,9 +24,11 @@ const MarketItem = ({ itemName, itemPrice, itemCategory }) => {
                 <div className="row">
                     <p style={{textAlign: 'justify', textJustify: 'inter-word'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 </div>
+                {user && (
                 <div className="row">
                     <button>Borrow</button>
                 </div>
+                )}
             </div>
         </div>
     );
@@ -36,51 +38,52 @@ const MarketItem = ({ itemName, itemPrice, itemCategory }) => {
 const MarketItemWithData = ({ user, keyword }) => {
     const location = useLocation();
     const [marketItemData, setMarketItemData] = useState([]);
-    console.log("Called Market Item Data");
     useEffect(() => {
-        console.log("Called useEffect");
-        if (keyword === "") return;
         const fetchMarketItemData = async () => {
-            console.log("Called fetch");
-            const response = await fetch(`https://backend-dot-lendly-383321.wl.r.appspot.com/api/items/name/${keyword}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            // Check if any items match results
-            if (response.ok) {
-                const marketItemData = await response.json();
-                console.log(marketItemData);
-
-                // Remove items that are already borrowed
-                marketItemData.forEach(marketItem => {
-                    if (marketItem.status == true) {
-                        const index = marketItemData.indexOf(marketItem);
-                        marketItemData.splice(index, 1);
-                    }
+            if (keyword === "") {}
+            else {
+                const response = await fetch(`https://backend-dot-lendly-383321.wl.r.appspot.com/api/items/name/${keyword}`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
                 });
 
-                if (marketItemData.length > 0) {
-                    setMarketItemData(marketItemData);
+                // Check if any items match results
+                if (response.ok) {
+                    const marketItemData = await response.json();
+                    console.log(marketItemData);
+
+                    // Remove items that are already borrowed
+                    marketItemData.forEach(marketItem => {
+                        if (marketItem.status == true) {
+                            const index = marketItemData.indexOf(marketItem);
+                            marketItemData.splice(index, 1);
+                        }
+                    });
+
+                    if (marketItemData.length > 0) {
+                        setMarketItemData(marketItemData);
+                    }
+                    else {
+                        console.error('No results found');
+                    }
                 }
                 else {
                     console.error('No results found');
                 }
-            }
-            else {
-                console.error('No results found');
             }
         };
         fetchMarketItemData();
     }, [location]);
 
     return (
+        
         <div>
             {marketItemData.map((item) => (
                 <MarketItem
+                    user={user}
                     itemName={item.name}
                     itemPrice={item.insurancePrice}
                     //itemDesc={item.description}
