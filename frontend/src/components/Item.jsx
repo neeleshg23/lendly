4b899b7dd355f664496df8edf2a60e7d754c4ddc
Andrea Gonzalez <andreag4@usc.edu>
2023-04-23 21:58:58 -0700

    button test

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -2,32 +2,34 @@ import React, { useEffect, useState } from "react";
 import './../App.css';
 import image from "./../images/item_alt.png"
 
-const Item = ({ itemName, itemPrice }) => {
+const Item = ({ itemName, itemPrice, itemtype }) => {
     return (
         <div className="item">
-            <img src={image}/>
+            <img src={image} />
             <p>{itemName}</p>
             <p><b>${itemPrice}</b></p>
-            <button style={{marginBottom: 15 +'px'}}>Return Item</button>
+            {itemtype === "OWN" && (
+                <button style={{ marginBottom: 15 + "px" }}>Return Item</button>
+            )}
         </div>
     );
 };
 
-const ItemWithData = ({user, itemtype}) => {
+const ItemWithData = ({ user, itemtype }) => {
     const [itemData, setItemData] = useState([]);
     useEffect(() => {
         const fetchItemData = async () => {
             if (user) {
-                const userFetch = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/"+user.email);
+                const userFetch = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/" + user.email);
                 const userJson = await userFetch.json(); // userFetch response is JSON
                 const id = userJson.id; // accessing id value
 
-                if(itemtype == "OWN") {
-                    const response = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/"+id+"/owned-items");
+                if (itemtype == "OWN") {
+                    const response = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/" + id + "/owned-items");
                     const itemData = await response.json();
                     setItemData(itemData);
                 } else {
-                    const response = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/"+id+"/borrowed-items");
+                    const response = await fetch("https://backend-dot-lendly-383321.wl.r.appspot.com/api/users/" + id + "/borrowed-items");
                     const itemData = await response.json();
                     setItemData(itemData);
                 }
@@ -42,6 +44,7 @@ const ItemWithData = ({user, itemtype}) => {
                 <Item
                     itemName={item.name}
                     itemPrice={item.insurancePrice}
+                    itemtype={itemtype}
                 />
             ))}
         </div>
